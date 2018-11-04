09651adb3361a715b2dd77bd9c2b5c54d7210bc9
yuhan <719808205@qq.com>
2018-11-04 15:41:52 +0800

    .

---
+++ 905.Sort Array By Parity.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] sortArrayByParity(int[] A) {
+        return Arrays
+                .stream(A)
+                .boxed()
+                .sorted((a, b) -> Integer.compare(a % 2, b % 2))
+                .mapToInt(a -> a)
+                .toArray();
+    }
+}
